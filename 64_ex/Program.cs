863bb8b76f446ef6282af106defd2ed088572c22
muhammad <mukhamed.saidov.2002@mail.ru>
2023-07-22 15:42:43 +0400

    64

--- 64_ex/Program.cs
+++ 64_ex/Program.cs
@@ -1,6 +1,24 @@
-﻿/*
-Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-*/
+﻿/*  
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии. 
+N = 5 -> "5, 4, 3, 2, 1" 
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+*/ 
+ 
+using System; 
+using static System.Console; 
+Clear(); 
+ 
+Write("Введите конечное число: "); 
+int begin = int.Parse(ReadLine()); 
+Write("Введите N: "); 
+int n = int.Parse(ReadLine()); 
+ 
+WriteLine(PrintNumbers(begin, n)); 
+ 
+string PrintNumbers(int first, int last) 
+{; 
+    if (last == first) return last.ToString(); 
+    return (last + " " + PrintNumbers(first, last - 1)); 
+}
+
 
